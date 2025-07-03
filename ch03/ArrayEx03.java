070caa2131d530ab3a349f0ab21af39c1e2305eb
miraeKim <mirkim6086@naver.com>
2025-07-03 09:29:02 +0900

    Add files via upload

---
+++ ch03/ArrayEx03.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class ArrayEx03 {
+    public static void main(String[] args) {
+        //배열의 크기만 선언
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[3] = 3;
+        //nums 의0번째 위치에 5를 누적
+        nums[0] = nums[0] + 5;
+        for (int x : nums){
+            System.out.println(x);
+        }
+    }
+}
